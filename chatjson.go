8d5897cbd87ea42c88e27cdffef731285d8468d7
Ultra Derek <ultradereks@gmail.com>
2024-06-02 01:55:10 -0400

    main initial program

---
+++ chatjson.go
@@ -0,0 +1,58 @@
+package main
+
+/*
+type TwitchFeed struct {
+	Messages []Message
+}
+
+type Message struct {
+	Author      Author `json:"author"`
+	Messsage    string `json:"message"`
+	MessageType string `json:"message_type"`
+	TimeSeconds uint   `json:"time_in_seconds"`
+	TimeText    string `json:"time_text"`
+	Timestamp   uint   `json:"timestamp"`
+}
+
+type Author struct {
+	Badges      []Badge `json:"badges"`
+	Color       string  `json:"colour"`
+	DisplayName string  `json:"display_name"`
+	ID          uint    `json:"id"`
+	Name        string  `json:"name"`
+}
+
+type Badge struct {
+	ClickAction string `json:"clickAction"`
+	ClickURL    string `json:"clickURL"`
+	Name        string `json:"name"`
+	Title       string `json:"title"`
+	Version     uint8  `json:"version"`
+}
+type Emote struct {
+	// Images []Image `json:"images"`
+	ID        string `json:"id"`
+	Locations string `json:"locations"`
+	Name      string `json:"name"`
+}
+
+func CreateTwitchFeed() (x *TwitchFeed) {
+	return &TwitchFeed{}
+}
+*/
+/*
+//example url
+// https://static-cdn.jtvnw.net/badges/v1/ceb9f2bb-70ff-4e3a-80b4-51da68f5f4a2/2
+type Icon struct {
+	Height int `json:"height"`
+	Dems   int `json:"id"`
+	URL string `json:"url"`
+}
+//example url
+//https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_5801163bd6c240e8a3fd30cc86387acb/default/light/3.0
+type Image struct{
+	Height uint8 `json:"height"`
+	URL string `json:"url"`
+
+}
+*/
